a41a64361162bd14c284c7abd6e63240a7b0ccfb
[Nahom <nahomhaile72@gmail.com>
2020-06-23 06:01:40 -0400

    Done

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # RobertIncomeGoal
+Assignment:
+
+Robert is working on his 5-year goal : To increase his current $62,000 annual income by 10% each year.
+
+Pseudocode:
+
+    BEGIN
+        Set currentAnnualIncome = 62000
+        Set increaseByNPercent = 10
+        Set goalDuration = 5
+        Set rateInDecimal = increaseByNPercent/100
+        Set fifthYearAnnualIncome = currentAnnualIncome(1+rateInDecimal)^goalDuration
+        
+        Set output = "Annual income on the fifth year: $" + fifthYearAnnualIncome
+        Display output
+    END
--- src/FifthYearIncomeGoal.java
+++ src/FifthYearIncomeGoal.java
@@ -1,2 +1,17 @@
+import java.text.NumberFormat;
+
 public class FifthYearIncomeGoal {
+    public static void main(String [] args){
+        int currentAnnualIncome = 62000;
+        int increaseByNPercent = 10;
+        int goalDuration = 5;
+        double rateInDecimal = increaseByNPercent/100.0;
+        double fifthYearAnnualIncome = currentAnnualIncome * Math.pow((1+rateInDecimal), goalDuration);
+
+
+        System.out.println("Annual income on the fifth year: " +
+                NumberFormat.getCurrencyInstance().format(fifthYearAnnualIncome));
+
+
+    }
 }
